a1324d3df54a11d5240bd1cce17ed20373c9169f
kumaran <kumaran26skr@gmail.com>
2021-10-18 13:51:54 +0200

    Initial commit

---
+++ src/test/java/com/aiportal/springapp/SpringAppTests.java
@@ -0,0 +1,13 @@
+package com.aiportal.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
